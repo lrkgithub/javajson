d057da1305e7f18ff616ba9b14709dd94c3ad8f4
lrkgithub <r915658977@qq.com>
2018-06-12 23:07:39 +0800

    add from TODO

--- src/main/java/com/lrk/javajson/core/JsonMapper.java
+++ src/main/java/com/lrk/javajson/core/JsonMapper.java
@@ -1,10 +1,14 @@
 package main.java.com.lrk.javajson.core;
 
+import main.java.com.lrk.javajson.annotation.Property;
 import main.java.com.lrk.javajson.parse.JsonValue;
 import main.java.com.lrk.javajson.parse.elment.JsonArray;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JsonMapper {
 
@@ -22,13 +26,60 @@ public class JsonMapper {
 
         if (this.jsonValue.getClass() == JsonArray.class) {
 
-            ArrayList arrayLsit = new ArrayList();
+            ArrayList<Object> arrayList = new ArrayList<Object>();
 
             List<JsonValue> jsonValueList = ((JsonArray)jsonValue).getList();
 
+            for (JsonValue jv : jsonValueList) {
+
+                arrayList.add(fromSingleJson(jv, clazz));
+            }
+            return arrayList;
+        }
+
+        if (clazz.getClassLoader() == this.getClass().getClassLoader()) {
+
+            return fromSingleJson(this.jsonValue, clazz);
+            
         }
 
         return null;
     }
 
+    private Object fromSingleJson(JsonValue jv, Class clazz) {
+
+        Field[] fields = clazz.getDeclaredFields();
+
+        Map<String, Field> alliesNames = prepareFileds(fields);
+
+
+
+        return null;
+    }
+
+    private Map<String, Field> prepareFileds(Field[] fields) {
+
+        Map<String, Field> map = new HashMap<String, Field>();
+
+        for (Field field : fields) {
+
+            Property property = field.getAnnotation(Property.class);
+
+            String name;
+
+            if (null != property) {
+                name = property.value();
+            } else {
+                name = "";
+            }
+
+            if ("".equals(name)) {
+                name = field.getName();
+            }
+
+            map.put(name, field);
+        }
+
+        return map;
+    }
 }
